c2992f66322323f10955efed90ce4b165cc21946
ryan-school2020 <ryan.khursheedps@gmail.com>
2022-02-13 09:02:04 +0530

    added validation schemas

---
+++ models/validation/schema.js
@@ -0,0 +1,38 @@
+const joi = require("joi");
+
+const userSchema = joi
+  .object({
+    name: joi.string().required(),
+    email: joi.string().email().required(),
+    profileImage: joi.string().required(),
+    c,
+  })
+  .unknown();
+
+const categorySchema = joi
+  .object({
+    name: joi.string().required(),
+    limits: joi
+      .object()
+      .keys({
+        weekly: joi.number().required(),
+        monthly: joi.number().required(),
+      })
+      .required(),
+  })
+  .unknown();
+
+const expenseSchema = joi
+  .object({
+    name: joi.string().required(),
+    amount: joi.number().required(),
+  })
+  .unknown();
+
+const commentSchema = joi
+  .object({
+    text: joi.string().required(),
+  })
+  .unknown();
+
+module.exports = { userSchema,categorySchema,expenseSchema,commentSchema };
